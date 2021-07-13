3c95c3df8130b233efff7ccb31c5c022fd79792e
Ross Barnowski <rossbar@berkeley.edu>
2021-07-13 14:24:51 +0300

    Ultrabasic benchmarking.
     - Add pandas to reqs for benchmarking.

---
+++ bench/bench.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from npreadtext import _loadtxt
+
+# Prepare csv file
+a = np.random.rand(100_000, 5)
+np.savetxt("test.csv", a, delimiter=",")
+
+print("loadtxt:")
+get_ipython().run_line_magic('timeit', 'b = np.loadtxt("test.csv", delimiter=",")')
+print("read_csv")
+get_ipython().run_line_magic('timeit', 'c = pd.read_csv("test.csv", delimiter=",")')
+print("read_csv w/ honest floats")
+get_ipython().run_line_magic(
+    'timeit', 'c = pd.read_csv("test.csv", delimiter=",", float_precision="round_trip")'
+)
+print("_loadtxt")
+get_ipython().run_line_magic('timeit', 'd = _loadtxt("test.csv", delimiter=",")')
